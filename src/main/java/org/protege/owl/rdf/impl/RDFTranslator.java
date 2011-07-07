99d5f65b26b4cb1906811f2b28de4d7c79a6dc6d
Timothy Redmond <tredmond@stanford.edu>
2011-07-07 00:37:33 +0000

    the rdf translator almost seems to be working?

--- src/main/java/org/protege/owl/rdf/impl/RDFTranslator.java
+++ src/main/java/org/protege/owl/rdf/impl/RDFTranslator.java
@@ -1,46 +1,95 @@
 package org.protege.owl.rdf.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.coode.owlapi.rdf.model.AbstractTranslator;
-import org.openrdf.model.Literal;
+import org.openrdf.model.BNode;
 import org.openrdf.model.Resource;
-import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 
-public class RDFTranslator extends AbstractTranslator<Value, Resource, URI, Literal> {
+public class RDFTranslator extends AbstractTranslator<Value, Resource, org.openrdf.model.URI, org.openrdf.model.Literal> {
+	private Repository repository;
+	private org.openrdf.model.URI axiomResource;
+	private Map<Object, BNode> bnodeMap = new HashMap<Object, BNode>();
+	private RepositoryConnection connection;
 
-	public RDFTranslator(OWLOntologyManager manager, OWLOntology ontology, boolean useStrongTyping) {
-		super(manager, ontology, useStrongTyping);
-		// TODO Auto-generated constructor stub
+	public RDFTranslator(Repository repository, OWLOntologyManager manager, OWLOntology ontology) {
+		super(manager, ontology, true);
+		this.repository = repository;
+	}
+	
+	public org.openrdf.model.URI startTransaction() throws RepositoryException {
+		ValueFactory factory = repository.getValueFactory();
+		axiomResource = factory.createURI(OwlTripleStoreImpl.NS + "/" + UUID.randomUUID().toString().replace('-', '_'));
+		bnodeMap.clear();
+		connection = repository.getConnection();
+		return axiomResource;
+	}
+	
+	public void finishTransaction(boolean success) throws RepositoryException {
+		if (success) {
+			connection.commit();
+		}
+		else {
+			connection.rollback();
+		}
+		connection.close();
+	}
+	
+	public RepositoryConnection getConnection() {
+		return connection;
 	}
 
 	@Override
-	protected Resource getResourceNode(IRI IRI) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	protected org.openrdf.model.URI getResourceNode(IRI iri) {
+		ValueFactory factory = repository.getValueFactory();
+		return factory.createURI(iri.toString());
 	}
 
 	@Override
-	protected URI getPredicateNode(IRI IRI) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	protected org.openrdf.model.URI getPredicateNode(IRI iri) {
+		ValueFactory factory = repository.getValueFactory();
+		return factory.createURI(iri.toString());
 	}
 
 	@Override
-	protected Resource getAnonymousNode(Object key) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	protected BNode getAnonymousNode(Object key) {
+		return repository.getValueFactory().createBNode();
 	}
 
 	@Override
-	protected Literal getLiteralNode(OWLLiteral literal) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	protected org.openrdf.model.Literal getLiteralNode(OWLLiteral literal) {
+		ValueFactory factory = repository.getValueFactory();
+		if (literal.isRDFPlainLiteral() && literal.getLang() != null) {
+			return factory.createLiteral(literal.getLiteral(), literal.getLang());
+		}
+		else if (literal.isRDFPlainLiteral()) {
+			return factory.createLiteral(literal.getLiteral());
+		}
+		else {
+			return factory.createLiteral(literal.getLiteral(), factory.createURI(literal.getDatatype().getIRI().toString()));
+		}
 	}
 
 	@Override
-	protected void addTriple(Resource subject, URI pred, Value object) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	protected void addTriple(Resource subject, org.openrdf.model.URI pred, Value object) {
+		try {
+			connection.add(subject, pred, object, axiomResource);
+		}
+		catch (RepositoryException e) {
+			throw new RepositoryRuntimeException(e);
+		}
 	}
 
 }
