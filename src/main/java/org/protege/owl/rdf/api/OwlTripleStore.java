367c9817a198f9ff2eb8980481b683b8b9980fab
Timothy Redmond <tredmond@stanford.edu>
2011-07-07 00:36:25 +0000

    added exceptions and a hasAxiom method

--- src/main/java/org/protege/owl/rdf/api/OwlTripleStore.java
+++ src/main/java/org/protege/owl/rdf/api/OwlTripleStore.java
@@ -2,15 +2,17 @@ package org.protege.owl.rdf.api;
 
 import org.openrdf.model.Resource;
 import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryException;
 import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 public interface OwlTripleStore {
 
 	Repository getRepository();
 	
-	Resource addAxiom(OWLAxiom axiom);
+	Resource addAxiom(OWLAxiom axiom) throws RepositoryException;
 	
-	void removeAxiom(OWLAxiom axiom);
+	void removeAxiom(OWLAxiom axiom) throws RepositoryException;
 	
-	
+	boolean hasAxiom(OWLAxiom axiom) throws RepositoryException;
 }
